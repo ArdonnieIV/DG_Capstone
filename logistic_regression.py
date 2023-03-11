0bbe38f46c869ac3a6ab16eca044d27cdd3c65f5
Fizza Ahmed <fizza0ahmed@gmail.com>
2023-03-11 17:43:30 -0500

    logistic regression doesn't converge :(

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,10 +1,8 @@
 from sklearn.linear_model import LogisticRegression
-import dataloader
+from dataloader.dataloader import DataLoader
 
 
-# retrieve these from helper functions
-labels = None
-features = None
+
 
 # retrieve train/test split from helper functions as well
 X_train = None
@@ -13,8 +11,14 @@ y_train = None
 X_test = None
 y_test = None
 
+dl = DataLoader()
+X_train, y_train, X_test, y_test = dl.get_train_test_split()
+
 logistic_model = LogisticRegression(class_weight="balanced", max_iter=1000)
 logistic_model.fit(X_train, y_train)
 
 train_accuracy = logistic_model.score(X_train, y_train)
 test_accuracy  = logistic_model.score(X_test, y_test)
+
+print(train_accuracy)
+print(test_accuracy)
